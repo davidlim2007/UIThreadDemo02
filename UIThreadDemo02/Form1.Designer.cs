3304edb57ff0c07935af0798a1b0dd53aa8984c6
davidlim2007 <lim.david2007@gmail.com>
2017-07-30 19:08:51 +0800

    First commit
    Adding all project files.

---
+++ UIThreadDemo02/Form1.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace UIThreadDemo02
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnCreateThread = new System.Windows.Forms.Button();
+            this.btnCreateThreadWithParameter = new System.Windows.Forms.Button();
+            this.btnCreateThreadAnonymousEntryPoint = new System.Windows.Forms.Button();
+            this.btnThreadAbort = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnCreateThread
+            // 
+            this.btnCreateThread.Location = new System.Drawing.Point(26, 188);
+            this.btnCreateThread.Name = "btnCreateThread";
+            this.btnCreateThread.Size = new System.Drawing.Size(348, 30);
+            this.btnCreateThread.TabIndex = 0;
+            this.btnCreateThread.Text = "Create Thread";
+            this.btnCreateThread.UseVisualStyleBackColor = true;
+            this.btnCreateThread.Click += new System.EventHandler(this.btnCreateThread_Click);
+            // 
+            // btnCreateThreadWithParameter
+            // 
+            this.btnCreateThreadWithParameter.Location = new System.Drawing.Point(26, 226);
+            this.btnCreateThreadWithParameter.Name = "btnCreateThreadWithParameter";
+            this.btnCreateThreadWithParameter.Size = new System.Drawing.Size(348, 30);
+            this.btnCreateThreadWithParameter.TabIndex = 1;
+            this.btnCreateThreadWithParameter.Text = "Create Thread with Parameter";
+            this.btnCreateThreadWithParameter.UseVisualStyleBackColor = true;
+            this.btnCreateThreadWithParameter.Click += new System.EventHandler(this.btnCreateThreadWithParameter_Click);
+            // 
+            // btnCreateThreadAnonymousEntryPoint
+            // 
+            this.btnCreateThreadAnonymousEntryPoint.Location = new System.Drawing.Point(26, 265);
+            this.btnCreateThreadAnonymousEntryPoint.Name = "btnCreateThreadAnonymousEntryPoint";
+            this.btnCreateThreadAnonymousEntryPoint.Size = new System.Drawing.Size(348, 30);
+            this.btnCreateThreadAnonymousEntryPoint.TabIndex = 2;
+            this.btnCreateThreadAnonymousEntryPoint.Text = "Create Thread with Anonymous Method";
+            this.btnCreateThreadAnonymousEntryPoint.UseVisualStyleBackColor = true;
+            this.btnCreateThreadAnonymousEntryPoint.Click += new System.EventHandler(this.btnCreateThreadAnonymousEntryPoint_Click);
+            // 
+            // btnThreadAbort
+            // 
+            this.btnThreadAbort.Location = new System.Drawing.Point(26, 306);
+            this.btnThreadAbort.Name = "btnThreadAbort";
+            this.btnThreadAbort.Size = new System.Drawing.Size(348, 30);
+            this.btnThreadAbort.TabIndex = 3;
+            this.btnThreadAbort.Text = "Thread Abortion";
+            this.btnThreadAbort.UseVisualStyleBackColor = true;
+            this.btnThreadAbort.Click += new System.EventHandler(this.btnThreadAbort_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(402, 392);
+            this.Controls.Add(this.btnThreadAbort);
+            this.Controls.Add(this.btnCreateThreadAnonymousEntryPoint);
+            this.Controls.Add(this.btnCreateThreadWithParameter);
+            this.Controls.Add(this.btnCreateThread);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCreateThread;
+        private System.Windows.Forms.Button btnCreateThreadWithParameter;
+        private System.Windows.Forms.Button btnCreateThreadAnonymousEntryPoint;
+        private System.Windows.Forms.Button btnThreadAbort;
+    }
+}
+
---
+++ UIThreadDemo02/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace UIThreadDemo02
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCreateThread_Click(object sender, EventArgs e)
+        {
+            // The following code will initiate a new thread with 
+            // WorkThreadFunction() as the entry point method.
+            // Note that WorkThreadFunction() does not take any parameters
+            // hence the use of the ThreadStart delegate.
+            Thread thread = new Thread(new ThreadStart(WorkThreadFunction));
+
+            // Note that the above call can be simplified to :
+            // Thread thread = new Thread(WorkThreadFunction);
+            // This is because the compiler is able to deduce that
+            // WorkThreadFunction() does not take any parameter and so
+            // the ThreadStart delegate type is implied.
+            thread.Start();
+        }
+
+        // WorkThreadFunction() is the entry-point to a thread.
+        // i.e. when the thread starts, it starts with WorkThreadFunction().
+        public void WorkThreadFunction()
+        {
+            int i = 0;
+            while (true)
+            {
+                i++;
+                Thread.Sleep(1000);
+
+                if (i >= 10)
+                {
+                    break;
+                }
+            }
+        }
+
+        private void btnCreateThreadWithParameter_Click(object sender, EventArgs e)
+        {
+            // The following code will initiate a new thread with 
+            // WorkThreadFunctionWithParameter() as the entry point method.
+            // Note that WorkThreadFunctionWithParameter() takes one parameter
+            // hence the use of the ParameterizedThreadStart delegate.
+            Thread thread = new Thread(new ParameterizedThreadStart(WorkThreadFunctionWithParameter));
+
+            // Note that the above call can be simplified to :
+            // Thread thread = new Thread(WorkThreadFunctionWithParameter);
+            // This is because the compiler is able to deduce that
+            // WorkThreadFunctionWithParameter() takes a parameter and so
+            // the ParameterizedThreadStart delegate type is implied.
+            // Note that if thread.Start() is called without any parameter,
+            // then the parameter is set to null.
+            thread.Start(5000);
+        }
+
+        // WorkThreadFunctionWithParameter() is the entry-point to a thread.
+        // i.e. when the thread starts, it starts with WorkThreadFunctionWithParameter().
+        // Note also that this method takes a parameter but it could be set to null.
+        public void WorkThreadFunctionWithParameter(object objParameter)
+        {
+            int iDuration = 0;
+
+            // Check whether objParameter is null in case Thread.Start()
+            // is called without any parameters.
+            if (objParameter != null)
+            {
+                iDuration = (int)objParameter;
+            }
+
+            Thread.Sleep(iDuration);
+        }
+
+        private void btnCreateThreadAnonymousEntryPoint_Click(object sender, EventArgs e)
+        {
+            // The following initiates a new thread with an anonymous entry-point
+            // method (i.e. an entry point method without any name).
+            // Note that the body of the anonymous method will only be executed
+            // when the thread officially starts.
+            Thread thread = new Thread((object objParameter) =>
+            {
+                // Thread entry point method.
+                string strText = (string)objParameter;
+                File.AppendAllText("test.txt", strText);
+            });
+
+            thread.Start("My text\r\n");
+        }
+
+        // This button handler will initiate a thread with 
+        // WorkThreadFunctionWithTryCatch() as the entry point.
+        // After initiating the thread, this handler will 
+        // block for 1 second thereby allowing the
+        // WorkThreadFunctionWithTryCatch() thread to start.
+        // Immediately after that, this handler will instruct
+        // the thread to abort.
+        private void btnThreadAbort_Click(object sender, EventArgs e)
+        {
+            Thread thread = new Thread(new ThreadStart(WorkThreadFunctionWithTryCatch));
+            thread.Start();
+
+            // Halt the current thread for 1 second. This will allow the
+            // OS to allow other threads to run.
+            // Hence the WorkThreadFunctionWithTryCatch()
+            // thread will start running.
+            Thread.Sleep(1000);
+
+            // Once WorkThreadFunctionWithTryCatch() starts running, 
+            // we proceed to abort the thread by calling thread.Abort().
+            // Doing so will cause a ThreadAbortException exception 
+            // to be thrown inside the thread.
+            thread.Abort();
+        }
+
+        // WorkThreadFunctionWithTryCatch() is the entry-point to a thread.
+        // i.e. when the thread starts, it starts with WorkThreadFunctionWithTryCatch().
+        public void WorkThreadFunctionWithTryCatch()
+        {
+            try
+            {
+                // Do a simple block for 10 seconds.
+                Thread.Sleep(10000);
+            }
+            catch (ThreadAbortException taex)
+            {
+                // Code will reach here if the Thread.Abort()
+                // methid is called on this thread.
+                MessageBox.Show(taex.Message);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                // The finally clause will always be executed
+                // on method exit.
+                MessageBox.Show("Thread ends.");
+            }
+        }
+    }
+}
